51ffd9377cd45d1f40a8a7edaa492e6bfd4720c4
ywf154 <ywf154@qq.com>
2024-10-26 13:05:14 +0300

    dist del

--- dist/assets/index-DkGCGJrm.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./index-BAbb37-O.js";import{r as s,o as n,c as t,d as l,f as c}from"./index-C_8758mc.js";const m={__name:"index",setup(u){const r=s("/prod-api/druid/login.html");return(p,e)=>(n(),t("div",null,[l(c(o),{src:r.value,"onUpdate:src":e[0]||(e[0]=a=>r.value=a)},null,8,["src"])]))}};export{m as default};
--- dist/assets/index-Uasr2ZNc.js
+++
@@ -1,1 +0,0 @@
-import{o as s,c,u as n,a as u}from"./index-C_8758mc.js";const i={__name:"index",setup(p){const e=n(),t=u(),{params:o,query:a}=e,{path:r}=o;return t.replace({path:"/"+r,query:a}),(_,m)=>(s(),c("div"))}};export{i as default};
